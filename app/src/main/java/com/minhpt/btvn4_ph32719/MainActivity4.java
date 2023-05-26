80dba56fec07d93b242c825ec73ca171c3d81c8c
Minh <phamminh20804@gmail.com>
2023-05-26 16:24:01 +0700

    Initial commit

---
+++ app/src/main/java/com/minhpt/btvn4_ph32719/MainActivity4.java
@@ -0,0 +1,14 @@
+package com.minhpt.btvn4_ph32719;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
